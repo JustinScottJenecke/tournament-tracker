dd3dea2a69fcb313dea54a6220dc8d833c986155
JustinScottJenecke <justinjenecke@gmail.com>
2024-02-27 21:58:33 +0200

    adds docs to MatchupEntryEntity properties

--- TrackerLibrary/MatchupEntryEntity.cs
+++ TrackerLibrary/MatchupEntryEntity.cs
@@ -8,8 +8,28 @@ namespace TrackerLibrary
 {
     public class MatchupEntryEntity
     {
+        /// <summary>
+        ///     represents one team in the matchup
+        /// </summary>
         public TeamEntity TeamCompeting { get; set; }
+
+        /// <summary>
+        ///     represwents the score for this particular team
+        /// </summary>
         public double Score { get; set; }
+
+        /// <summary>
+        ///     represents the matchup that this team came from as the winner
+        /// </summary>
         public MatchupEntity ParentMatchup { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="initialScore"></param>
+        public MatchupEntryEntity(double initialScore)
+        {
+            
+        }
     }
 }
